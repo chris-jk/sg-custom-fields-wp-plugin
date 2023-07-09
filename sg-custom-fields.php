f20f141124a4e5e9879626125914e5d1d6ebd768
chris-jk <edetail@yahoo.com>
2023-07-09 12:31:12 -0700

    added API

--- sg-custom-fields.php
+++ sg-custom-fields.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * Plugin Name: sg Custom Field
- * Plugin URI: https://docs.metabox.io
+ * Plugin Name: SG Custom Fields and REST API
  * Description: plugin for strain guide custom fields.
  * Version: 1.0
  * Author: ya boy cash
- * Author URI: https://docs.metabox.io
  * License: GPL2
  */
 
@@ -122,3 +120,84 @@ function hcf_save_meta_box($post_id)
 }
 
 add_action('save_post', 'hcf_save_meta_box');
+
+
+
+
+/**
+ * Register custom fields with REST API.
+ */
+function hcf_register_rest_fields()
+{
+  $fields = [
+    'hcf_aka',
+    'hcf_user_ratings',
+    'hcf_strain_type',
+    'hcf_THC',
+    'hcf_CBG',
+    'hcf_CBD',
+    'hcf_dominate_terp',
+    'hcf_other_terp_1',
+    'hcf_other_terp_2',
+    'hcf_flav_1',
+    'hcf_flav_2',
+    'hcf_flav_3',
+    'hcf_feel_1',
+    'hcf_feel_2',
+    'hcf_feel_3',
+    'hcf_help_1',
+    'hcf_help_2',
+    'hcf_help_3',
+    'hcf_neg_1',
+    'hcf_neg_2',
+    'hcf_neg_3',
+    'hcf_seed_link',
+    'hcf_parent_1',
+    'hcf_parent_2',
+    'hcf_child_1',
+    'hcf_child_2',
+    'hcf_grow_dif',
+    'hcf_grow_avg_hight',
+    'hcf_grow_avg_yeild',
+    'hcf_grow_time',
+  ];
+
+  foreach ($fields as $field) {
+    register_rest_field(
+      'post',
+      $field,
+      [
+        'get_callback' => 'hcf_get_rest_field',
+        'update_callback' => 'hcf_update_rest_field',
+        'schema' => null,
+      ]
+    );
+  }
+}
+add_action('rest_api_init', 'hcf_register_rest_fields');
+
+/**
+ * Get custom field value for REST API.
+ *
+ * @param array $object Details of current REST API request.
+ * @param string $field_name Field name.
+ * @param WP_REST_Request $request Current REST API request.
+ * @return mixed
+ */
+function hcf_get_rest_field($object, $field_name, $request)
+{
+  return get_post_meta($object['id'], $field_name, true);
+}
+
+/**
+ * Update custom field value through REST API.
+ *
+ * @param mixed $value New value of the field.
+ * @param object $object The object from the response.
+ * @param string $field_name Name of the field.
+ * @return bool|int
+ */
+function hcf_update_rest_field($value, $object, $field_name)
+{
+  return update_post_meta($object->ID, $field_name, sanitize_text_field($value));
+}
